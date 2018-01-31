d3d75c5ac4403a92453e84e607b90ffdc6542b72
Andrzej Duś <andrzej.dus@gmail.com>
2018-01-31 19:07:47 +0100

    Improve main function flow

--- src/main.cpp
+++ src/main.cpp
@@ -6,47 +6,45 @@
 
 int main() {
     Bitmap bitmap;
-    if (bitmap.load("assets/test.bmp") == 0) {
-        BitmapInfoPrinter bitmapInfo(&bitmap);
-        bitmapInfo.printType();
-        bitmapInfo.printSize();
-        bitmapInfo.printOffset();
-        bitmapInfo.printInfoHeaderSize();
-        bitmapInfo.printWidth();
-        bitmapInfo.printHeight();
-        bitmapInfo.printColorPlanesCount();
-        bitmapInfo.printBitsPerPixel();
-        bitmapInfo.printCompressionMethod();
-        bitmapInfo.printColorsCount();
+    if (bitmap.load("assets/test.bmp") != 0) {
+        printf("Could not load bitmap\n");
+        return 1;
     }
+
+    BitmapInfoPrinter bitmapInfo(&bitmap);
+    bitmapInfo.printType();
+    bitmapInfo.printSize();
+    bitmapInfo.printOffset();
+    bitmapInfo.printInfoHeaderSize();
+    bitmapInfo.printWidth();
+    bitmapInfo.printHeight();
+    bitmapInfo.printColorPlanesCount();
+    bitmapInfo.printBitsPerPixel();
+    bitmapInfo.printCompressionMethod();
+    bitmapInfo.printColorsCount();
 
     Mode13h mode13h;
 
-    if (mode13h.init() == 0) {
-
-        mode13h.setPalette((uint32_t *) bitmap.getPalette());
-
-        for (int x = 0; x < VGA_SCREEN_WIDTH; x++) {
-            for (int y = 0; y < VGA_SCREEN_HEIGHT; y++) {
-                mode13h.drawPixel(x, y, bitmap.getPixelColor(x, y));
-            }
-        }
-
-//        for (int x = 0; x < mode13h.getScreenDimensions().width; x++) {
-//            for (int y = 0; y < mode13h.getScreenDimensions().height; y++) {
-//                mode13h.drawPixel(x, y, y % 256);
-//            }
-//        }
-        mode13h.update();
-
-        printf("OK!\n");
-        getchar();
-
-        mode13h.exit();
-    } else {
+    if (mode13h.init() != 0) {
         printf("VGA initialization error!\n");
         getchar();
+        return 1;
     }
+
+    mode13h.setPalette((uint32_t *) bitmap.getPalette());
+
+    for (int x = 0; x < VGA_SCREEN_WIDTH; x++) {
+        for (int y = 0; y < VGA_SCREEN_HEIGHT; y++) {
+            mode13h.drawPixel(x, y, bitmap.getPixelColor(x, y));
+        }
+    }
+
+    mode13h.update();
+
+    printf("OK!\n");
+    getchar();
+
+    mode13h.exit();
 
     return 0;
 }
